ff570827e4da6a08bd963b61dad5c8a99d90c2d0
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2021-03-21 22:05:19 +1100

    first commit

---
+++ html/script.js
@@ -0,0 +1,63 @@
+let interval;
+const setHour = (newNum) => {
+    const hours = document.getElementById("hour");
+    if (newNum)
+        hours.innerText = `${newNum}h `;
+    else
+        hours.innerText = "";
+    return hours;
+};
+const setMin = (newNum) => {
+    const min = document.getElementById("min");
+    if (newNum)
+        min.innerText = `${newNum >= 10 ? newNum : `${newNum}`}m `;
+    else if (newNum === 0)
+        min.innerText = "";
+    else
+        min.innerText = "0m";
+    return min;
+};
+const setSecs = (newNum) => {
+    const sec = document.getElementById("sec");
+    sec.innerText = `${newNum >= 10 ? newNum : `${newNum}`}s `;
+    return sec;
+};
+function start({ hours, mins: minutes, secs: seconds, }) {
+    clearInterval(interval);
+    if (minutes >= 60) {
+        hours += Number((minutes / 60).toFixed(0));
+        minutes = Number((minutes % 60).toFixed(0));
+    }
+    if (seconds >= 60) {
+        minutes += Number((seconds / 60).toFixed(0));
+        seconds = Number((seconds % 60).toFixed(0));
+    }
+    //const [hours, minutes, seconds] = [
+    setHour(hours || 0); //,
+    setMin(minutes || 0); //,
+    setSecs(seconds || 0); //,
+    // ];
+    interval = setInterval(() => {
+        if (seconds <= 0 && minutes <= 0 && hours >= 1) {
+            hours--;
+            seconds = 59;
+            minutes = 59;
+        }
+        else if (seconds <= 0 && minutes <= 1) {
+            seconds = 59;
+            minutes = 0;
+        }
+        else if (seconds <= 0) {
+            seconds = 59;
+            minutes--;
+        }
+        else {
+            seconds--;
+        }
+        setHour(hours || 0);
+        setMin(minutes || 0);
+        setSecs(seconds || 0);
+        if (seconds === 0 && minutes === 0 && hours === 0)
+            return clearInterval(interval);
+    }, 1000);
+}
